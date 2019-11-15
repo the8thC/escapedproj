64742a4322ce1e6057e00c63293ef2bcd6036d34
Лысый Олег <minatych@gmail.com>
2019-11-15 16:20:54 +0300

    chore(Agent): test agent

--- ProjE/Assets/Scripts/Test.cs
+++ ProjE/Assets/Scripts/Test.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CharacterController _characterController;
+    
+    [Range(0, 100)]
+    public float speed;
+    
     void Start()
     {
-        
+        _characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var moveDirection = Vector3.left;
+        moveDirection *= speed / 1000;
+        _characterController.Move( moveDirection );
     }
 }
